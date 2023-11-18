ef9b6e3cf18d99b56e669ec207ebb83b66107dfa
frankiensteinne <nobleza.frankie@gmail.com>
2023-11-19 03:27:25 +0800

    fixed Ace value determiner, added game and win tracker

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 ### Day 10 of 100 Days of Python
-Check out 100 Days of Code: The Complete Python Pro Bootcamp for 2023 with Dr. Angela Yu https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/
+Check out 100 Days of Code: The Complete Python Pro Bootcamp for 2023 
+with Dr. Angela Yu https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/
 
 
-House Rules for the game
+*** House Rules for the game ***
 ## The deck is unlimited in size. 
 ## There are no jokers. 
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
+
+*** Added Functionalities ***
+## Number of Games Tracker
+## Win Tracker
--- main.py
+++ main.py
@@ -2,59 +2,117 @@ from art import logo
 import random
 import os
 
+player_win = 0
+computer_win = 0
+number_of_games = 0
+
 def clear():
     os.system('clear')
 
-def face_to_num(cards, current_score):
-    """Converts face cards to numerical values."""
-    for i in range(len(cards)):
-        if cards[i] == "Jack" or cards[i] == "Queen" or cards[i] == "King":
-            cards[i] = 10
-        elif cards[i] == "Ace":
-            # Decide whether to treat Ace as 1 or 11 based on the current hand
-            if current_score + 11 <= 21:
-                cards[i] = 11
-            else:
-                cards[i] = 1
-    
+def face_to_num(cards):
+    """Converts face cards to numerical values and handles Ace values."""
+    updated_cards = []
 
-def play_blackjack():
+    for card in cards:
+        if card in ["Jack", "Queen", "King"]:
+            updated_cards.append(10)
+        elif card == "Ace":
+            # Use 11 as the default value for Ace
+            updated_cards.append(11)
+        else:
+            updated_cards.append(card)
+
+    # Check if there are Aces in the hand and adjust their value to 1 if needed
+    while sum(updated_cards) > 21 and 11 in updated_cards:
+        updated_cards[updated_cards.index(11)] = 1
+
+    return updated_cards
+
+
+def determine_winner(player_score, computer_score):
+    global player_win
+    global computer_win
+    if player_score > 21:
+        computer_win += 1
+        print("You bust, you lose.")
+    elif computer_score > 21:
+        player_win += 1
+        print("Computer busts, you win.")
+    elif player_score == 21:
+        player_win += 1
+        print("You have a BLACKJACK! You win.")
+    elif computer_score == 21:
+        print("Computer has a BLACKJACK! You lose.")
+        computer_win += 1
+    elif player_score > computer_score:
+        print("You win!")
+    elif player_score < computer_score:
+        computer_win += 1
+        print("You lose.")
+    else:
+        print("It's a tie.")
+
+def play_blackjack(first_play=True):
     """Starts Game"""
     cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
     
     player_cards = [random.choice(cards), random.choice(cards)]
-    face_to_num(player_cards, sum(player_cards))
-    player_score = sum(player_cards)
+    player_cards_new = face_to_num(player_cards)
+    player_score = sum(player_cards_new)
 
     computer_cards = [random.choice(cards), random.choice(cards)]
-    face_to_num(computer_cards, sum(computer_cards))
-    computer_score = sum(computer_cards)
+    computer_cards_new = face_to_num(computer_cards)
+    computer_score = sum(computer_cards_new)
 
-    print(logo)
+    global player_win
+    global computer_win
+    global number_of_games
+
+    if first_play:
+        print(logo)  # Display logo only during the first play
+
     print(f"Your cards: {player_cards}, current score: {player_score}")
     print(f"Computer's first card: {computer_cards[0]}")
 
     while True:
-        new_card = input("Type \"y\" to get a new card, type \"n\" to pass: ").lower()
+        new_card = input("Type 'y' to get a new card, type 'n' to pass: ").lower()
         if new_card == "y":
-            player_cards.append(random.randrange(0,len(cards)))
-            face_to_num(player_cards, sum(player_cards))
-            player_score = sum(player_cards)
-            break
+            player_cards.append(random.choice(cards))
+            player_cards_new = face_to_num(player_cards)
+            player_score = sum(player_cards_new)
+            print(f"Your cards: {player_cards}, current score: {player_score}")
+            if player_score > 21:
+                break
         elif new_card == "n":
-            break  
+            break
         else:
             print("Please input a valid answer.")
 
+    while computer_score < 17:
+        new_card = random.choice(cards)
+        print(f"Computer drew: {new_card}")
+        computer_cards.append(new_card)
+        computer_cards_new = face_to_num(computer_cards)
+        computer_score = sum(computer_cards_new)
+
+    print(f"Your final hand: {player_cards}, final score: {player_score}")
+    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+    determine_winner(player_score, computer_score)
+    number_of_games += 1
+    print(f'''You have played {number_of_games} times.
+Computer won {computer_win} times.
+You won {player_win} times.'''
+    )
+
+# Use flag to determine if it's the first play
+first_play_flag = True
 
 while True:
-    play = input("Do you want to play a game of Blackjack? y/n: ").lower()
-
-    if play == "y":
-        play_blackjack()
-        break
-    elif play == "n":
+    play_again = input(f"Do you want to {'play a game of Blackjack' if first_play_flag else 'play again'}? y/n: ").lower()
+    if play_again != "y":
         clear()
-        break  
+        print("Thank you for playing!")
+        break
     else:
-        print("Please input a valid answer.")
+        play_blackjack(first_play=first_play_flag)  # Pass first_play flag
+        first_play_flag = False  # Set the flag to False for subsequent plays
